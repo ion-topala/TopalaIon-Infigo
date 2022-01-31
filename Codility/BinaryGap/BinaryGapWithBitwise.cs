c946e81d3dde164154b37d0ae6bdc38f5f1a98b7
Topala Ion <ion.topala@iis.utm.md>
2022-01-31 17:50:55 +0200

    Bitwise Operator

---
+++ Codility/BinaryGap/BinaryGapWithBitwise.cs
@@ -0,0 +1,34 @@
+﻿namespace Codility;
+
+public class BinaryGapWithBitwise
+{
+    public int solution(int N)
+    {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            var current = (N >> i) & 1;
+            if (current == 1)
+            {
+                if (count >= max)
+                {
+                    max = count;
+                    count = 0;
+                }
+            }
+            else
+            {
+                count++;
+            }
+        }
+        Console.WriteLine("The max {0}",max);
+        return max;
+    }
+
+    static public void Main(String[] args)
+    {
+        BinaryGapWithBitwise obj = new BinaryGapWithBitwise();
+        obj.solution(529);
+    }
+}
