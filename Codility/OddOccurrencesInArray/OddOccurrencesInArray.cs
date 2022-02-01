56bc169f357eb7590f0648920c1c5fe8c808b4bd
Topala Ion <ion.topala@iis.utm.md>
2022-02-01 14:12:18 +0200

    add method SolutionOddOccurrenceXOR

---
+++ Codility/OddOccurrencesInArray/OddOccurrencesInArray.cs
@@ -0,0 +1,49 @@
+﻿class OddOccurrencesInArray
+{
+    public int SolutionOddOccurrence(int[] givenArray)
+    {
+            int oddNumber = givenArray[0];
+            int globalCounter = 0;
+
+            for (int i = 0; i < givenArray.Length; i++)
+            {
+                int counter = 1;
+                
+                for (int j = 0; j < givenArray.Length ; j++)
+                {
+                    if (i != j)
+                    {
+                        if (givenArray[i] == givenArray[j])
+                        {
+                            counter++;
+                        }
+                    }
+                }
+
+                if (counter % 2 == 1)
+                {
+                    globalCounter = givenArray[i];
+                }
+            }
+
+            return globalCounter;
+    }
+    public int SolutionOddOccurrenceXOR(int[] givenArray)
+    {
+        int oddNumber = givenArray[0];
+        
+        for (int i = 1; i < givenArray.Length; i++)
+        {
+            oddNumber = oddNumber ^ givenArray[i];
+        }
+        return oddNumber;
+    }
+    
+
+    static public void Main(String[] args)
+    {
+        OddOccurrencesInArray obj = new OddOccurrencesInArray();
+        int[] numbersArgument = {9, 3, 9, 3, 9, 7, 9};
+        Console.WriteLine(obj.SolutionOddOccurrenceXOR(numbersArgument));
+    }
+}
