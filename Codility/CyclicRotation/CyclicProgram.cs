7bf69e4f000d9eae827443ace7b3596cc73ac2cb
Topala Ion <ion.topala@iis.utm.md>
2022-02-01 11:25:41 +0200

    modified variables in CyclingProgram

--- Codility/CyclicRotation/CyclicProgram.cs
+++ Codility/CyclicRotation/CyclicProgram.cs
@@ -2,39 +2,39 @@
 
 public class CyclicProgram
 {
-    public int[] solution(int[] A, int K)
+    public int[] SolutionCyclicProgram(int[] givenArray, int numbersOfShifting)
     {
-        if (A.Length > 0)
+        if (givenArray.Length > 0)
         {
-            int N = A.Length;
+            int lengthArray = givenArray.Length;
             
-            for (int j = 0; j < K; j++)
+            for (int j = 0; j < numbersOfShifting; j++)
             {
-                int globalTemp = A[0];
+                int globalTemp = givenArray[0];
                 
-                for (int i = 0; i < N; i++)
+                for (int i = 0; i < lengthArray; i++)
                 {
                     if (i == 0)
                     {
-                        A[0] = A[N - 1];
+                        givenArray[0] = givenArray[lengthArray - 1];
                     }
                     else
                     {
-                        int temp = A[i];
-                        A[i] = globalTemp;
+                        int temp = givenArray[i];
+                        givenArray[i] = globalTemp;
                         globalTemp = temp;
                     }
                 }
             }
         }
 
-        return A;
+        return givenArray;
     }
 
     static public void Main(String[] args)
     {
         CyclicProgram obj = new CyclicProgram();
         int[] numbersArgument = {};
-        obj.solution(numbersArgument, 3);
+        obj.SolutionCyclicProgram(numbersArgument, 3);
     }
 }
